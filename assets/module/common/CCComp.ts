90f4b7a0303d8c92774325525c26743fe7219411
dgflash <dgflash@qq.com>
2022-09-25 19:21:31 +0800

    .

--- assets/module/common/CCComp.ts
+++ assets/module/common/CCComp.ts
@@ -14,16 +14,15 @@ const { ccclass, property } = _decorator;
 /** 
  * 游戏显示对象组件
  * 
- * 使用方法：
+ * 功能介绍：
  * 1. 对象拥有 cc.Component 组件功能与 ecs.Comp 组件功能
  * 2. 对象自带全局事件监听、释放、发送全局消息功能
- * 3. 对象管理的所有节点摊平，直接通过节点名获取cc.Node对象（节点名不能有重名）
+ * 3. 对象管理的所有节点摊平，直接通过节点名获取cc.Node对象
  * 
  * 应用场景
  * 1. 网络游戏，优先有数据对象，然后创建视图对象，当释放视图组件时，部分场景不希望释放数据对象
  * 
  * @example
-// 注册显示对象组件
 @ccclass('RoleViewComp')
 @ecs.register('RoleView', false)
 export class RoleViewComp extends CCComp {
