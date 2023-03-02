a1c92ae5c339f98ed945255f0fbad5fe407d2b36
dgflash <dgflash@qq.com>
2023-03-02 21:52:38 +0800

    EffectFinishedRelease支持Spine动画播放完成后回收到对象池中

--- assets/libs/animator-effect/EffectFinishedRelease.ts
+++ assets/libs/animator-effect/EffectFinishedRelease.ts
@@ -2,10 +2,10 @@
  * @Author: dgflash
  * @Date: 2022-08-19 15:36:08
  * @LastEditors: dgflash
- * @LastEditTime: 2022-09-22 14:55:58
+ * @LastEditTime: 2023-03-01 18:28:55
  */
 
-import { Animation, Component, ParticleSystem, _decorator } from 'cc';
+import { Animation, Component, ParticleSystem, _decorator, sp } from 'cc';
 import { oops } from '../../core/Oops';
 import { EffectEvent } from './EffectEvent';
 
@@ -18,31 +18,50 @@ export class EffectFinishedRelease extends Component {
     private maxDuration: number = 0;
 
     protected onEnable() {
-        let anims: Animation[] = this.node.getComponentsInChildren(Animation);
-        anims.forEach(animator => {
-            let aniName = animator.defaultClip?.name;
-            if (aniName) {
-                let aniState = animator.getState(aniName);
-                if (aniState) {
-                    aniState.speed = 1;
-                    let duration = aniState.duration;
-                    this.maxDuration = duration > this.maxDuration ? duration : this.maxDuration;
-                }
+        // SPINE动画
+        let spine = this.getComponent(sp.Skeleton);
+        if (spine) {
+            // 播放第一个动画
+            let json = (spine.skeletonData.skeletonJson as any).animations;
+            for (var name in json) {
+                spine.setCompleteListener(this.onRecovery.bind(this));
+                spine.setAnimation(0, name, false);
+                break;
             }
-            animator.play();
-        });
+        }
+        else {
+            // COCOS动画
+            let anims: Animation[] = this.node.getComponentsInChildren(Animation);
+            if (anims.length > 0) {
+                anims.forEach(animator => {
+                    let aniName = animator.defaultClip?.name;
+                    if (aniName) {
+                        let aniState = animator.getState(aniName);
+                        if (aniState) {
+                            aniState.speed = 1;
+                            let duration = aniState.duration;
+                            this.maxDuration = duration > this.maxDuration ? duration : this.maxDuration;
+                        }
+                    }
+                    animator.play();
+                });
+                this.scheduleOnce(this.onRecovery.bind(this), this.maxDuration);
+            }
+            // 粒子动画
+            else {
+                let particles: ParticleSystem[] = this.node.getComponentsInChildren(ParticleSystem);
+                particles.forEach(particle => {
+                    particle.simulationSpeed = 1;
+                    particle.clear();
+                    particle.stop();
+                    particle.play()
 
-        let particles: ParticleSystem[] = this.node.getComponentsInChildren(ParticleSystem);
-        particles.forEach(particle => {
-            particle.simulationSpeed = 1;
-            particle.clear();
-            particle.stop();
-            particle.play()
-
-            let duration: number = particle.duration;
-            this.maxDuration = duration > this.maxDuration ? duration : this.maxDuration;
-        });
-        this.scheduleOnce(this.onRecovery.bind(this), this.maxDuration);
+                    let duration: number = particle.duration;
+                    this.maxDuration = duration > this.maxDuration ? duration : this.maxDuration;
+                });
+                this.scheduleOnce(this.onRecovery.bind(this), this.maxDuration);
+            }
+        }
     }
 
     private onRecovery() {
