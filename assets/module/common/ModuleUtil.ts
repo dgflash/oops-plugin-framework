c09363f8c8b632440aecb69fc13c87839383b8f8
donggang <>
2024-02-26 18:09:02 +0800

    优化

---
+++ assets/module/common/ModuleUtil.ts
@@ -0,0 +1,28 @@
+import { Node, __private } from "cc";
+import { oops } from "../../core/Oops";
+import { UICallbacks } from "../../core/gui/layer/Defines";
+import { ecs } from "../../libs/ecs/ECS";
+import { CCComp } from "./CCComp";
+import { CCVMParentComp } from "./CCVMParentComp";
+import { CompType } from "../../libs/ecs/ECSModel";
+
+export class ModuleUtil {
+    public static addView<T extends CCVMParentComp | CCComp>(
+        ent: ecs.Entity,
+        ctor: __private._types_globals__Constructor<T> | __private._types_globals__AbstractedConstructor<T>,
+        uiId: number,
+        uiArgs: any = null) {
+        var uic: UICallbacks = {
+            onAdded: (node: Node, params: any) => {
+                var comp = node.getComponent(ctor) as ecs.Comp;
+                ent.add(comp);
+            }
+        };
+        oops.gui.open(uiId, uiArgs, uic);
+    }
+
+    public static removeView(ent: ecs.Entity, ctor: CompType<ecs.IComp>, uiId: number, isDestroy: boolean = true) {
+        ent.remove(ctor);
+        oops.gui.remove(uiId, isDestroy);
+    }
+}
