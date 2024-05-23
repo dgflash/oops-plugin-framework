a66b7d39a734e4fe4dcf3ab4cb8936760be8d9cd
donggang <>
2024-05-23 11:12:08 +0800

    ModuleUtil.addViewUiAsync支持加载失败处理

--- assets/module/common/ModuleUtil.ts
+++ assets/module/common/ModuleUtil.ts
@@ -48,6 +48,9 @@ export class ModuleUtil {
                     var comp = node.getComponent(ctor) as ecs.Comp;
                     ent.add(comp);
                     resolve(node);
+                },
+                onLoadFailure: () => {
+                    resolve(null);
                 }
             };
             oops.gui.open(uiId, uiArgs, uic);
