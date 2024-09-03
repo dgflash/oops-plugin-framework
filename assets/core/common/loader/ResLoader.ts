e5ba11b69015027b85eb762e43c10f165d27c20a
dgflash <dgflash@qq.com>
2024-09-03 13:57:45 +0800

    游戏资源管理模块添加四个全局属性 1、下载时的最大并发数 2、下载时每帧可以启动的最大请求数 3、失败重试次数 4、重试的间隔时间

--- assets/core/common/loader/ResLoader.ts
+++ assets/core/common/loader/ResLoader.ts
@@ -15,7 +15,7 @@ interface ILoadResArgs<T extends Asset> {
 }
 
 /** 
- * 游戏资管理 
+ * 游戏资源管理
  * 1、加载默认resources文件夹中资源
  * 2、加载默认bundle远程资源
  * 3、主动传递bundle名时，优先加载传递bundle名资源包中的资源
@@ -23,6 +23,38 @@ interface ILoadResArgs<T extends Asset> {
 export class ResLoader {
     /** 全局默认加载的资源包名 */
     defaultBundleName: string = "resources";
+
+    /** 下载时的最大并发数 - 项目设置 -> 项目数据 -> 资源下载并发数，设置默认值；初始值为15 */
+    get maxConcurrency() {
+        return assetManager.downloader.maxConcurrency;
+    }
+    set maxConcurrency(value) {
+        assetManager.downloader.maxConcurrency = value;
+    }
+
+    /** 下载时每帧可以启动的最大请求数 - 默认值为15 */
+    get maxRequestsPerFrame() {
+        return assetManager.downloader.maxRequestsPerFrame;
+    }
+    set maxRequestsPerFrame(value) {
+        assetManager.downloader.maxRequestsPerFrame = value;
+    }
+
+    /** 失败重试次数 - 默认值为0 */
+    get maxRetryCount() {
+        return assetManager.downloader.maxRetryCount;
+    }
+    set maxRetryCount(value) {
+        assetManager.downloader.maxRetryCount = value;
+    }
+
+    /** 重试的间隔时间，单位为毫秒 - 默认值为2000毫秒 */
+    get retryInterval() {
+        return assetManager.downloader.retryInterval;
+    }
+    set retryInterval(value) {
+        assetManager.downloader.retryInterval = value;
+    }
 
     /**
      * 加载远程资源
