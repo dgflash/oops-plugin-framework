90c4195a66552ebad22f516b965080ff4e33fb72
dgflash <dgflash@qq.com>
2025-09-04 20:19:59 +0800

    打开MVVM在编辑器模式下运行

--- assets/libs/model-view/VMEnv.ts
+++ assets/libs/model-view/VMEnv.ts
@@ -1,9 +1,9 @@
-import { EDITOR } from "cc/env";
+import { EDITOR_NOT_IN_PREVIEW } from "cc/env";
 
 /** VM组件环境验证 */
 export class VMEnv {
     /** 编辑状态 */
     static get editor() {
-        return EDITOR;
+        return EDITOR_NOT_IN_PREVIEW;
     }
 }
