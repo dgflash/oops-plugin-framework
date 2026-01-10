2c5e2452e6d700702008c98198a909feeef704df
dgflash <dgflash@qq.com>
2026-01-10 19:50:20 +0800

    修复框架项目中导入路径与API文档生成插件路径冲突

--- temp/declarations/cc.d.ts
+++ temp/declarations/cc.d.ts
@@ -1,5 +1,5 @@
 
-    /// <reference path="D:\cocos-dashboard-editors\Creator\3.8.4\resources\resources\3d\engine\bin\.declarations\cc.d.ts"/>
+    /// <reference path="D:\Cocos Editors\Creator\3.8.8\resources\resources\3d\engine\bin\.declarations\cc.d.ts"/>
     
     /**
      * @deprecated Global variable `cc` was dropped since 3.0. Use ES6 module syntax to import Cocos Creator APIs.
--- temp/declarations/cc.env.d.ts
+++ temp/declarations/cc.env.d.ts
@@ -55,11 +55,6 @@ declare module 'cc/env'{
 	export const WECHAT_MINI_PROGRAM: boolean;
 
 	/**
-	 * Running in the baidu's mini game.
-	 */
-	export const BAIDU: boolean;
-
-	/**
 	 * Running in the xiaomi's quick game.
 	 */
 	export const XIAOMI: boolean;
@@ -100,19 +95,19 @@ declare module 'cc/env'{
 	export const HUAWEI: boolean;
 
 	/**
-	 * Running in the cocosplay.
+	 * Running in the migu's quick game.
 	 */
-	export const COCOSPLAY: boolean;
+	export const MIGU: boolean;
 
 	/**
-	 * Running in the qtt's quick game.
+	 * Running in the honor's quick game.
 	 */
-	export const QTT: boolean;
+	export const HONOR: boolean;
 
 	/**
-	 * Running in the linksure's quick game.
+	 * Running in the cocos runtime.
 	 */
-	export const LINKSURE: boolean;
+	export const COCOS_RUNTIME: boolean;
 
 	/**
 	 * Running in the editor.
